982456b0247d9e3e5b27e5fecdde00e36e47ca1b
michael <dikandumichael@gmail.com>
2024-03-13 09:37:54 +0100

    java files

--- src/TicTacToeGame/Cell.java
+++ src/TicTacToeGame/Cell.java
@@ -1,16 +1,16 @@
-package TicTacToeGame;
-    public enum Cell {
-        X("X"),
-        O("O"),
-        EMPTY("   ");
-        private final String value;
-
-        Cell (String value){
-            this.value = value;
-        }
-        public String getValue() {
-            return value;
-        }
-    }
-
-
+//package TicTacToeGame;
+//    public enum Cell {
+//        X("X"),
+//        O("O"),
+//        EMPTY("   ");
+//        private final String value;
+//
+//        Cell (String value){
+//            this.value = value;
+//        }
+//        public String getValue() {
+//            return value;
+//        }
+//    }
+//
+//

9950e475718dfd185acdb4b319b451249d8a4179
michael <dikandumichael@gmail.com>
2024-03-14 07:40:54 +0100

    java files

--- src/SevenSegmentDisplay/Display.java
+++ src/SevenSegmentDisplay/Display.java
@@ -4,6 +4,6 @@ import static SevenSegmentDisplay.SevenSegment.display;
 
 public class Display {
     public static void main(String[] args) {
-        System.out.println(display("11110111"));
+        System.out.println(display("11111111"));
     }
 }

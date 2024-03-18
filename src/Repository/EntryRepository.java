192bbe354a11ca382c9603757ee9c235b16f5aa1
michael <dikandumichael@gmail.com>
2024-03-18 22:48:49 +0100

    java files

--- src/Repository/EntryRepository.java
+++ src/Repository/EntryRepository.java
@@ -1,6 +1,5 @@
 package Repository;
 
-import Model.Diary;
 import Model.Entry;
 
 import java.util.List;
@@ -9,7 +8,7 @@ public interface EntryRepository {
     Entry save(Entry entry);
     List<Entry> findAll();
 
-    Entry findByUsername(String username);
+    boolean findByUsername(String username);
 
     Entry findById(int id);
     long countEntry();

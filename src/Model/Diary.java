97d9d72fcaa9f46ee2fe3e84ee3a4c997a0ba9b1
michael <dikandumichael@gmail.com>
2024-03-19 20:13:23 +0100

    java files

--- src/Model/Diary.java
+++ src/Model/Diary.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Diary {
     private String username;
@@ -40,10 +41,26 @@ public class Diary {
     public void unLock() {
         isLocked = false;
     }
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Diary diary = (Diary) object;
+        return Objects.equals(username, diary.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
     public void deleteEntry(int entryNumber) {
         entries.removeIf(entryId-> entryId.getId() == entryNumber);
     }
     public void createEntry(Entry entry) {
         entries.add(entry);
+    }
+    @Override
+    public String toString() {
+        return "Diary with username: " + username;
     }
 }

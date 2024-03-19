94d77b0fb0ba84bc4d8652c9ceda6c105ad1a182
michael <dikandumichael@gmail.com>
2024-03-20 00:07:52 +0100

    java files

--- src/Services/EntryServicesImplement.java
+++ src/Services/EntryServicesImplement.java
@@ -1,12 +1,15 @@
 package Services;
 
-import Model.Diary;
 import Model.Entry;
 import Repository.EntryRepositoryImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntryServicesImplement implements EntryServices{
 
     private EntryRepositoryImpl entryRepository = new EntryRepositoryImpl();
+    private List<Entry>entries = new ArrayList<>();
     @Override
     public void createEntry(Entry entry) {
        entryRepository.save(entry);
@@ -31,10 +34,9 @@ public class EntryServicesImplement implements EntryServices{
     public long numberOfEntries() {
         return entryRepository.findAll().size();
     }
-
     @Override
-    public void findAll() {
-
+    public List<Entry> findAll(Entry entry) {
+        return entries;
     }
 
     @Override

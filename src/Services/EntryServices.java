932dbf1f705932efcdb91f218dc38d607ef61af4
michael <dikandumichael@gmail.com>
2024-03-20 00:08:07 +0100

    java files

--- src/Services/EntryServices.java
+++ src/Services/EntryServices.java
@@ -3,13 +3,16 @@ package Services;
 import Model.Diary;
 import Model.Entry;
 
+import java.util.List;
+
 public interface EntryServices {
     void createEntry(Entry entry);
     void deleteEntry(Entry entry);
     void findById(String title);
     void save(Entry entry);
     long numberOfEntries();
-    void findAll();
+
+    List<Entry> findAll(Entry entry);
 
     void findById();
 }

72f67efbf312b9c2fea462700ec638c7dd8303a1
michael <dikandumichael@gmail.com>
2024-03-19 08:59:21 +0100

    java files

--- test/Services/DiaryServicesImplementTest.java
+++ test/Services/DiaryServicesImplementTest.java
@@ -1,8 +1,34 @@
 package Services;
 
+import Model.Diary;
+import dtos.request.RegisterRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiaryServicesImplementTest {
+    private DiaryServicesImplement diaryServicesImplement;
+    @BeforeEach
+    public void initializer(){
+        diaryServicesImplement = new DiaryServicesImplement();
+    }
+    @Test
+    public void testToLogin(){
+        RegisterRequest request = new RegisterRequest();
+        request.setUsername("username");
+        request.setPassword("password");
+        Diary diary = new Diary();
+        diary.unLock();
+        assertFalse(diary.isLocked());
+    }
+    @Test
+    public void testToRegister(){
+        RegisterRequest request = new RegisterRequest();
+        request.setPassword("password");
+        request.setUsername("username");
+        diaryServicesImplement.register(request);
 
+    }
 
 }

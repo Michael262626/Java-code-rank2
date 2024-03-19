ecc7dd9ef9f38d683a9455c7aa4017bc1ddc52eb
michael <dikandumichael@gmail.com>
2024-03-19 20:10:07 +0100

    java files

--- src/dtos/request/LoginRequest.java
+++ src/dtos/request/LoginRequest.java
@@ -1,14 +1,14 @@
 package dtos.request;
 
-public class RegisterRequest {
+public class LoginRequest {
         private String username;
         private String password;
-        public RegisterRequest(String username,String password){
+        public LoginRequest(String username, String password){
             this.username = username;
             this.password = password;
         }
 
-    public RegisterRequest() {
+    public LoginRequest() {
 
     }
 

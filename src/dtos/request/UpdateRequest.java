6e553ac8064b464cdf0c0e78ad1e410ee0ad9b7b
michael <dikandumichael@gmail.com>
2024-03-19 19:20:48 +0100

    java files

---
+++ src/dtos/request/UpdateRequest.java
@@ -0,0 +1,41 @@
+package dtos.request;
+
+public class UpdateRequest {
+    public int getId() {
+        return id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int id;
+    private String name;
+    private String title;
+    private String body;
+
+}

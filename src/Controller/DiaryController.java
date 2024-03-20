0a3e73a022dc2714f5cf8c60a71865889b811c94
michael <dikandumichael@gmail.com>
2024-03-20 22:00:58 +0100

    java files

---
+++ src/Controller/DiaryController.java
@@ -0,0 +1,67 @@
+package Controller;
+
+import Exceptions.*;
+import Services.DiaryServicesImplement;
+import Services.EntryServicesImplement;
+import dtos.request.DeleteRequest;
+import dtos.request.LoginRequest;
+
+public class DiaryController {
+    private final DiaryServicesImplement diaryServicesImplement = new DiaryServicesImplement();
+    public String registerUser(LoginRequest request){
+        try{
+        diaryServicesImplement.register(request);
+        return "Registration was successful";
+    }catch (UserNameExistException e){
+            return"UserName exist";
+        }
+    }
+    public String login(String name, String password){
+        try{
+            diaryServicesImplement.login(name, password);
+            return "Logged in successfully";
+        }catch (InvalidPasswordException | InvalidUserNameException e){
+            return (e.getMessage());
+        }
+    }
+    public String logout(String name){
+        try{
+            diaryServicesImplement.logout(name);
+            return "Logged out successfully";
+        }catch (InvalidUserNameException | DiaryNotFoundException e){
+            return (e.getMessage());
+        }
+    }
+    public String findEntry(String title){
+        try{
+            diaryServicesImplement.findEntry(title);
+            return "Enjoy.....";
+        }catch (EntryNotFoundException e){
+            return (e.getMessage());
+        }
+    }
+    public String findDiary(String username){
+        try{
+            diaryServicesImplement.findDiaryByUsername(username);
+            return "Diary found";
+        }catch (InvalidUserNameException e){
+            return (e.getMessage());
+        }
+    }
+    public String deleteEntry(String title){
+        try{
+            diaryServicesImplement.deleteAnEntry(title);
+            return "Entry deleted successfully";
+        }catch (EntryNotFoundException e){
+            return (e.getMessage());
+        }
+    }
+    public String deleteDiary(DeleteRequest request){
+        try{
+            diaryServicesImplement.deleteDiary(request);
+            return "Diary is removed";
+        }catch (DiaryNotFoundException e){
+            return (e.getMessage());
+        }
+    }
+}

54c089aa12228f1e4b7872565366cc7d19cedefe
michael <dikandumichael@gmail.com>
2024-03-20 01:36:37 +0100

    java files

--- src/dtos/request/LoginRequest.java
+++ src/dtos/request/LoginRequest.java
@@ -3,9 +3,20 @@ package dtos.request;
 public class LoginRequest {
         private String username;
         private String password;
-        public LoginRequest(String username, String password){
+        private boolean isLocked = true;
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public LoginRequest(String username, String password, boolean isLocked){
             this.username = username;
             this.password = password;
+            this.isLocked = isLocked;
         }
 
     public LoginRequest() {

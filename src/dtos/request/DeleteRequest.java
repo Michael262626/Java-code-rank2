0feb94cde2827c9e85053cb64d0b17679cf36954
michael <dikandumichael@gmail.com>
2024-03-20 22:00:45 +0100

    java files

---
+++ src/dtos/request/DeleteRequest.java
@@ -0,0 +1,23 @@
+package dtos.request;
+
+public class DeleteRequest {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String password;
+}

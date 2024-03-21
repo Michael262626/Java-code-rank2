8914354a495b6b877f0aec21ea71427b30e08f34
michael <dikandumichael@gmail.com>
2024-03-21 06:15:24 +0100

    java files

--- src/UserClass/DiaryMain.java
+++ src/UserClass/DiaryMain.java
@@ -1,7 +1,10 @@
 package UserClass;
 
+import Controller.DiaryController;
 import Model.Diary;
+import Model.Entry;
 import Services.DiaryServicesImplement;
+import dtos.request.EntryCreation;
 import dtos.request.LoginRequest;
 
 import javax.swing.*;
@@ -9,8 +12,9 @@ import java.util.Scanner;
 
 public class DiaryMain {
     static Scanner scanner = new Scanner(System.in);
-    private static LoginRequest loginRequest = new LoginRequest();
-    private static final DiaryServicesImplement diaryServicesImplement = new DiaryServicesImplement();
+    private static final LoginRequest loginRequest = new LoginRequest();
+    private static final EntryCreation entryCreation=new EntryCreation();
+    private static final DiaryController diaryController = new DiaryController();
     private static String getInput(String prompt) {
         System.out.print(prompt);
         return scanner.nextLine();
@@ -34,12 +38,12 @@ public class DiaryMain {
     private static void register() {
         String username = getInput("Enter your name: ");
         String password = getInput("Enter your password: ");
-        diaryServicesImplement.register(new LoginRequest(username, password));
+        diaryController.registerUser(new LoginRequest(username, password));
         }
     private static void login(){
         String username = getInput("Enter your name: ");
         String password = getInput("Enter your password: ");
-        diaryServicesImplement.login(username, password);
+        diaryController.login(username, password);
     }
 
     private static void diaryMenu(){
@@ -48,14 +52,16 @@ public class DiaryMain {
                 <2> Update Entry
                 <3> Find Entry
                 <4> Delete Entry
-                <5> Home Page
-                <6> Exit... 
+                <5> Delete Diary
+                <6> Home Page
+                <7> Exit... 
                 """);
         switch(option){
             case "1" -> createEntry();
             case "2" -> updateEntry();
             case "3" -> findEntry();
             case "4" -> deleteEntry();
+            case "5" -> deleteDiary();
             case "5" -> mainMenu();
             case "6" -> System.exit(101);
             case default -> diaryMenu();
@@ -63,7 +69,9 @@ public class DiaryMain {
     }
 
     private static void createEntry() {
-
+        String title = getInput("Enter the title:\n ");
+        String body = getInput("Enter the body of your entry:\n ");
+        diaryController.createEntry(new EntryCreation(title, body));
     }
 }
 
